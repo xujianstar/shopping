4782f50b21fb0c4714df5314a9a0cd00ac917079
longmen <371300094@qq.com>
2020-07-23 16:34:25 +0800

    test1

--- chapter13/src/main/java/Test/TestThread.java
+++ chapter13/src/main/java/Test/TestThread.java
@@ -2,6 +2,27 @@ package Test;
 
 public class TestThread {
     public static void main(String[] args) {
-
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i=0;i<200;i++){
+                        try {
+                            System.out.println(i);
+                            Thread.currentThread().sleep(100*10);
+                            System.out.println(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            t.start();
+        System.out.println("主线程开始调用t.sleep()");
+        try {
+            t.sleep(1000*10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("主线程完成调用t.sleep()");
     }
 }
